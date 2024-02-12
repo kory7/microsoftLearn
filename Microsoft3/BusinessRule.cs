25bfb90a0c44df96151073a80e687cebff479b60
kory7 <jony.choi7@gmail.com>
2024-02-11 19:06:06 -0600

    Microsoft Learn Conditional rules

---
+++ Microsoft3/BusinessRule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Microsoft3
+{
+	public class BusinessRule
+	{
+		public string Permission { get; set; }
+		public int Level { get; set; }
+
+		public void PermissionMessage()
+		{
+			if(this.Permission.Contains("Admin"))
+			{
+				if (this.Level > 55)
+				{
+                    Console.WriteLine("Welcome, Super Admin user.");
+                }
+                else
+                {
+                    Console.WriteLine("Welcome, Admin user.");
+                }
+
+            }else if (this.Permission.Contains("Manager"))
+			{
+				if(this.Level >= 20)
+				{
+					Console.WriteLine("Contact an Admin for access.");
+				}
+				else
+				{
+					Console.WriteLine("You do not have sufficient privileges.");
+				}
+			}
+			else
+			{
+				Console.WriteLine("You do not have sufficient privileges.");
+			}
+		}
+	}
+
+}
+
---
+++ Microsoft3/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft3;
+
+Console.WriteLine("Hello, World!");
+
+string letterA = " A";
+string lettera = "a ";
+
+string starbucksOrder = "Caramel macchiato and cold brew";
+float ticket = 1000.10F;
+
+Console.WriteLine(letterA.ToLower().Trim() == lettera.ToLower().Trim());
+Console.WriteLine("Does the order contains cold brew? "+starbucksOrder.Contains("cold brew"));
+
+Console.WriteLine($"The total is: ${ticket} with discount of: {(ticket > 1000 ? "$100" : "$50")}");
+
+
+Random coin = new Random();
+
+//coin.Next(100);
+
+Console.WriteLine($"{ (coin.Next(100)>=50?"heads":"tails")}");
+
+BusinessRule NewManager = new BusinessRule();
+
+NewManager.Level = 60;
+NewManager.Permission = "Admin";
+
+NewManager.PermissionMessage();
